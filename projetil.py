d8e4de9e3efd80af74f47c9f8b685bb44f85d100
marcosf63 <marcosf63@gmail.com>
2018-06-14 20:26:11 -0300

    Melhorando codigo

--- projetil.py
+++ projetil.py
@@ -1,5 +1,4 @@
 import pygame
-import random
 import math
 
 # Definicoes da tela
@@ -10,6 +9,7 @@ pygame.display.set_caption('Projetil')
 resistenciaDoAr = 0.999
 elasticidade = 0.75
 
+# Tamanho eh a velocidade da particula
 def somaVetores((angulo1, tamanho1), (angulo2, tamanho2)):
   x = math.sin(angulo1) * tamanho1 + math.sin(angulo2) * tamanho2
   y = math.cos(angulo1) * tamanho1 + math.cos(angulo2) * tamanho2
@@ -20,10 +20,10 @@ def somaVetores((angulo1, tamanho1), (angulo2, tamanho2)):
   return (angulo, tamanho)
 
 class Particula:
-  def __init__(self, (x,y), tamanho, angulo):
+  def __init__(self, (x,y), raio, angulo):
     self.x = x
     self.y = y
-    self.tamanho = tamanho
+    self.raio = raio
     self.cor = (0,0,0)
     self.linha = 1
     self.velocidade = 0.01
@@ -36,7 +36,7 @@ class Particula:
     #self.velocidade *= resistenciaDoAr
    
   def display(self):
-    pygame.draw.circle(screen, self.cor, (int(self.x), int(self.y)), self.tamanho, self.linha)
+    pygame.draw.circle(screen, self.cor, (int(self.x), int(self.y)), self.raio, self.linha)
 
 
 
@@ -47,7 +47,7 @@ p1.velocidade = 3.5
 p2 = Particula((500 - 15, 15), 15, 0)
 p2.velocidade = 0
 
-g = 0.015
+g = 0.010
 
 
 running = True
